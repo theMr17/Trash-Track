9825e85abd9eed53e602728d95c8d8442d717a45
Rahul chakraborty <rahulchakraborty337@gmail.com>
2023-09-08 20:59:13 +0530

    Public,Dependencies, -no src

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+    content: ["./src/**/*.{html,js,jsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [],
+};
